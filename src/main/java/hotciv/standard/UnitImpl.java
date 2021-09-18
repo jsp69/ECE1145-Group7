9ae8bb8ba78950d58c6ecee591fbea0da497b035
Bella <veb20@pitt.edu>
2021-09-18 18:11:05 -0400

    more updates on 9-12, added UnitImpl

--- src/main/java/hotciv/standard/UnitImpl.java
+++ src/main/java/hotciv/standard/UnitImpl.java
@@ -1,2 +1,34 @@
-package hotciv.standard;public class UnitImpl {
+package hotciv.standard;
+
+import hotciv.framework.Unit;
+import hotciv.framework.Player;
+
+public class UnitImpl implements Unit {
+
+    Player owner;
+    int attack = 1;
+    int defense = 0;
+    int move = 0;
+
+    public String getTypeString() {return "archer";}
+
+    //Constructor assigning Owner
+    public UnitImpl(Player p){this.owner = p;}
+
+    //Accessor to get Owner
+    public Player getOwner(){return this.owner;}
+
+    //Accessor to get attacking strength
+    public int getAttackingStrength() {return attack; }
+
+    //Accessor to get defensive strength
+    public int getDefensiveStrength() {return defense;}
+
+    //Accessor to get move count
+    public int getMoveCount() {return move;}
+
 }
+
+
+
+
