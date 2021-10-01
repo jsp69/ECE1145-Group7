d8781b3b1eb5f6a5f3017cd4a67cbfad915d54bc
Bella <veb20@pitt.edu>
2021-10-01 00:54:18 -0400

    added GammaCiv.java, GammaCivImpl.java, TestGammaCiv.java...

--- src/test/java/hotciv/standard/TestGammaCiv.java
+++ src/test/java/hotciv/standard/TestGammaCiv.java
@@ -1,2 +1,23 @@
-package hotciv.standard;public class TestGammaCiv {
+package hotciv.standard;
+
+import hotciv.framework.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class TestGammaCiv {
+
+    private Game game;
+    /** Fixture for GammaCiv testing. */
+    @Before
+    public void setUp() {
+        game = new GameImpl(new GammaCivImpl());
+    }
+
+    @Test
+    public void settlerActionBuildCity() {
+        assertThat(game, is(notNullValue()));
+        assertThat(game.getPlayerInTurn(), is(Player.RED));
+    }
 }
