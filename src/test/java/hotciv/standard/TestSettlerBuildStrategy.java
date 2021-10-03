6377e1453ba5f6f9ad514cef9e40647235a86fba
Bella <veb20@pitt.edu>
2021-10-03 00:09:12 -0400

    finished GammaCiv

--- src/test/java/hotciv/standard/TestSettlerBuildStrategy.java
+++ src/test/java/hotciv/standard/TestSettlerBuildStrategy.java
@@ -1,9 +1,38 @@
 package hotciv.standard;
 
+import hotciv.framework.*;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 public class TestSettlerBuildStrategy {
 
+    private Game game;
+    /** Fixture for GammaCiv Strategy testing. */
+    @Before
     public void setUp() {
-        gc = new GammaCivImpl(new SettlerBuildStrategy());
+        game = new GammaCivImpl();
+    }
+
+    @Test
+    public void redSettlerBuildsCity() {
+        Position redSettler = new Position(4, 3);
+        assertThat(game, is(notNullValue()));
+        if (redSettler.equals("settler")) {
+            //Check if red owns built city at p
+            assertThat(game.getCityAt(redSettler).getOwner(), is(Player.RED));
+        }
+    }
+
+    @Test
+    public void blueSettlerBuildsCity() {
+        Position blueSettler = new Position(3, 4);
+        assertThat(game, is(notNullValue()));
+        if (blueSettler.equals("settler")) {
+            //Check if blue owns built city at p
+            assertThat(game.getCityAt(blueSettler).getOwner(), is(Player.BLUE));
+        }
     }
 
 }
