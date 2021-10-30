60e88158ff5624819b760388a4c4b9a856ac01c8
Bella <veb20@pitt.edu>
2021-10-30 19:32:19 -0400

    added getWinner(WinnerStrategyContext context) along with age methods

--- src/main/java/hotciv/standard/AlphaAging.java
+++ src/main/java/hotciv/standard/AlphaAging.java
@@ -1,8 +1,19 @@
 package hotciv.standard;
 
 import hotciv.framework.AgingStrat;
+import hotciv.framework.Player;
 
 public class AlphaAging implements AgingStrat {
+
+    private final int WINNING_AGE = -3000;
+
+    @Override
+    public Player getWinner(WinnerStrategyContext context) {
+        if(context.getAge() >= WINNING_AGE) {
+            return Player.RED;
+        }
+        return null;
+    }
     @Override
     public int increaseAge(int age) {
         return age + 100;
