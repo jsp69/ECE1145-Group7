15d8b6d2812236ca1c8bc0ddc349c298c5e9623e
Bella <veb20@pitt.edu>
2021-10-30 19:42:17 -0400

    tests for round 1 and 21 ZetaCiv

--- src/test/java/hotciv/standard/TestZetaCiv.java
+++ src/test/java/hotciv/standard/TestZetaCiv.java
@@ -1,2 +1,37 @@
-package hotciv.standard;public class TestZetaCiv {
-}
+package hotciv.standard;
+
+import hotciv.framework.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+
+public class TestZetaCiv {
+
+        private Game game;
+        WinStrat ws;
+        int roundsPassed;
+
+        @Before
+        public void setUp(){
+            game= new GameImpl(new ZetaFactory(roundsPassed, ws));
+        }
+
+        @Test public void roundLessThan20BetaWin() {
+            int roundCount = 1;
+            WinnerStrategyContext context = new WinnerStrategyContext();
+            Position rC=new Position(1,1);
+            Position bC=new Position(4,1);
+            WinStrat ws = new ZetaWin(new BetaWin(), new EpsilonWin(), roundCount);
+            assertThat(game.getWinner(), is(ws.getWinner(game)));
+        }
+
+       @Test public void roundGreaterThan20EpsilonWin() {
+            int roundCount = 21;
+           WinnerStrategyContext context = new WinnerStrategyContext();
+           Position rC=new Position(1,1);
+           Position bC=new Position(4,1);
+           WinStrat ws = new ZetaWin(new BetaWin(), new EpsilonWin(), roundCount);
+           assertThat(game.getWinner(), is(ws.getWinner(game)));
+        }
+    }
