213e74471f15432f1fc55566e070439d57adf431
lukespowerconverters <res175@pitt.edu>
2021-11-13 19:54:05 -0500

    updating Theta from Release6

--- src/main/java/hotciv/standard/ThetaMoveAttack.java
+++ src/main/java/hotciv/standard/ThetaMoveAttack.java
@@ -5,44 +5,144 @@ import hotciv.framework.*;
 import java.util.Objects;
 
 public class ThetaMoveAttack implements MoveAttackStrat {
+    Unit[][] u;
+    City[][] c;
+    Tile[][] t;
+
     @Override
     public boolean moveUnit(Position from, Position to, Game game, City[][] cityLoc, Unit[][] unitLoc) {
-        if (!Objects.equals(game.getUnitAt(from).getTypeString(), GameConstants.UFO)) {
-            //Ensure new position isn't mountains
-            if (Objects.equals(game.getTileAt(to).getTypeString(), GameConstants.MOUNTAINS)) {
-                return false;
-            }
-            // Ensure new position isn't oceans
-            if (Objects.equals(game.getTileAt(to).getTypeString(), GameConstants.OCEANS)) {
-                return false;
-            }
-        }
-        // Ensure it is the correct player's unit
-        if (game.getPlayerInTurn() != game.getUnitAt(from).getOwner()) {
+        //Update arrays
+        u = GameImpl.getUnitLoc();
+        c = GameImpl.getCityLoc();
+        t = GameImpl.getTileLoc();
+
+        //Ensure unit is not null
+        if (game.getUnitAt(from) == null) {
             return false;
         } else {
-            // Return true if unit was moved
-            int disCol = from.getColumn() - to.getColumn();
-            int disRow = from.getRow() - to.getRow();
-            boolean southNorth = (Math.abs(disCol) == 1);
-            boolean eastWest = (Math.abs(disRow) == 1);
-            boolean zeros = (disCol == 0) || (disRow == 0);
-            // Check that position is only a distance of 1 in any given direction
-            if ((southNorth ^ eastWest) && zeros) {
-                // Change position of unit
-                unitLoc[to.getRow()][to.getColumn()] = unitLoc[from.getRow()][from.getColumn()];
-                unitLoc[from.getRow()][from.getColumn()]= null;
-                //Change city ownership
-                if (game.getCityAt(to) != null) {
-                    if (game.getCityAt(to).equals(cityLoc[4][1]) && (game.getUnitAt(to).getOwner() == Player.RED)){
-                        ((CityImpl) (cityLoc[4][1])).setOwner(game.getUnitAt(to).getOwner());
-                    }
+            // Check for UFO
+            if (!Objects.equals(game.getUnitAt(from).getTypeString(), GameConstants.UFO)) {
+                //Ensure new position isn't mountains
+                if (Objects.equals(game.getTileAt(to).getTypeString(), GameConstants.MOUNTAINS)) {
+                    return false;
                 }
-                return true;
-            } else {
+                // Ensure new position isn't oceans
+                if (Objects.equals(game.getTileAt(to).getTypeString(), GameConstants.OCEANS)) {
+                    return false;
+                }
+            }
+
+            // Ensure it is the correct player's unit
+            if (game.getPlayerInTurn() != game.getUnitAt(from).getOwner()) {
                 return false;
             }
+
+            // Complete move
+            return canMove(from, to);
         }
     }
+
+    private boolean canMove(Position from, Position to) {
+        int x1 = from.getRow();
+        int y1 = from.getColumn();
+        int x2 = to.getRow();
+        int y2 = to.getColumn();
+        String unitType = u[x1][y1].getTypeString();
+
+        // Distance in columns, rows
+        int disCol = from.getColumn() - to.getColumn();
+        int disRow = from.getRow() - to.getRow();
+        int absDisCol = Math.abs(disCol);
+        int absDisRow = Math.abs(disRow);
+
+        // Type of move: vertical (v), horizontal (h), diagonal (d)
+        String moveType = null;
+        if (absDisCol == absDisRow) {
+            if (absDisCol == 2) { if (Objects.equals(unitType, GameConstants.UFO)) { moveType = "d"; } }
+            else if (absDisCol == 1) { if (!Objects.equals(unitType, GameConstants.UFO)) { moveType = "d"; } }
+            else { return false; }
+        }
+        else if (from.getRow() == to.getRow()) {
+            if (absDisCol == 2) { if (Objects.equals(unitType, GameConstants.UFO)) { moveType = "v"; } }
+            else if (absDisCol == 1) { if (!Objects.equals(unitType, GameConstants.UFO)) { moveType = "v"; } }
+            else { return false; }
+        }
+        else if (from.getColumn() == to.getColumn()){
+            if (absDisRow == 2) { if (Objects.equals(unitType, GameConstants.UFO)) { moveType = "h"; } }
+            else if (absDisRow == 1) { if (!Objects.equals(unitType, GameConstants.UFO)) { moveType = "h"; } }
+            else { return false; }
+        }
+        else { return false; }
+
+        int moveCount = u[x1][y1].getMoveCount();
+        if (moveCount == 2) {
+            assert moveType != null;
+            if (moveType.equals("d")) {
+                //up and left
+                if ((disCol > 0) && (disRow > 0)) {
+                    moveUnit1(x1, y1, x2+1, y2+1);
+                    moveUnit1(x2+1,y2+1,x2,y2);
+                }
+                //up and right
+                else if ((disCol > 0) && (disRow < 0)) {
+                    moveUnit1(x1, y1, x2-1, y2+1);
+                    moveUnit1(x2-1,y2+1,x2,y2);
+                }
+                //down and right
+                else if ((disCol < 0) && (disRow < 0)) {
+                    moveUnit1(x1, y1, x2-1, y2-1);
+                    moveUnit1(x2-1,y2-1,x2,y2);
+                }
+                //down and left
+                else {
+                    moveUnit1(x1, y1, x2+1, y2-1);
+                    moveUnit1(x2+1,y2-1,x2,y2);
+                }
+            }
+            else if (moveType.equals("v")) {
+                //up
+                if (disCol > 0) {
+                    moveUnit1(x1, y1, x2, y2+1);
+                    moveUnit1(x2,y2+1,x2,y2);
+                }
+                //down
+                else {
+                    moveUnit1(x1, y1, x2, y2-1);
+                    moveUnit1(x2, y2-1, x2, y2);
+                }
+            }
+            else {
+                //left
+                if (disRow > 0) {
+                    moveUnit1(x1, y1, x2-1, y2);
+                    moveUnit1(x2-1, y2, x2, y2);
+                }
+                //right
+                else {
+                    moveUnit1(x1, y1, x2+1, y2);
+                    moveUnit1(x2+1, y2, x2, y2);
+                }
+            }
+        }
+        else {
+            moveUnit1(x1, y1, x2, y2);
+        }
+
+        return true;
+    }
+
+    private void moveUnit1(int x1, int y1, int x2, int y2) {
+        u[x2][y2] = u[x1][y1];  // Copy unit
+        u[x1][y1] = null;       // Delete old unit
+    }
+
+    @Override
+    public City[][] getCitiesArray() { return c; }
+
+    @Override
+    public Tile[][] getTilesArray() { return t; }
+
+    @Override
+    public Unit[][] getUnitsArray() { return u; }
 }
 
--- src/main/java/hotciv/standard/ThetaUnitAction.java
+++ src/main/java/hotciv/standard/ThetaUnitAction.java
@@ -9,12 +9,6 @@ public class ThetaUnitAction implements UnitActionStrat {
     City[][] c;
     Tile[][] t;
 
-    public ThetaUnitAction() {
-       // u = GameImpl.getUnitLoc();
-       // c = GameImpl.getCityLoc();
-       // t = GameImpl.getTileLoc();
-    }
-
     @Override
     public City[][] getCitiesArray() { return c; }
 
@@ -25,67 +19,78 @@ public class ThetaUnitAction implements UnitActionStrat {
     public Unit[][] getUnitsArray() { return u; }
 
     @Override
-    public void performUnitActionAt(Position p, Game game) {
-        //System.out.print("innit");
-        c=((GameImpl)(game)).getCityLoc();
-        u=((GameImpl)(game)).getUnitLoc();
-        t=((GameImpl)(game)).getTileLoc();
+    public void performUnitActionAt(Position p) {
+        //Update arrays
+        u = GameImpl.getUnitLoc();
+        c = GameImpl.getCityLoc();
+        t = GameImpl.getTileLoc();
 
-        /*//Check if red settler
-        if (p ==  unitLoc[4][3]) {
-            //Remove from world
-            unitLoc[4][3] = null;
-            //Build city with red as owner
-            cityLoc[p.getRow()][p.getColumn()] = new CityImpl(Player.RED);
-            return cityLoc[p.getRow()][p.getColumn()];
-        }
-        //Check if blue settler
-        if (p == unitLoc[3][4]) {
-            //Remove from world
-            unitLoc[3][4] = null;
-            //Build city with blue as owner
-            cityLoc[p.getRow()][p.getColumn()] = new CityImpl(Player.BLUE);
-            return cityLoc[p.getRow()][p.getColumn()];
-        }
-        //Check if red archer
-        if (p == unitLoc[2][0]) {
-            //Check if already fortified
-            int oldDS = unitLoc[1][0].getDefensiveStrength()*2;
-            if(oldDS == unitLoc[2][0].getDefensiveStrength()) {
-                //Defensive strength stays same
-                unitLoc[1][0].getDefensiveStrength();
-            }
-            else {
-                //Double defensive strength
-                int ds = (unitLoc[2][0].getDefensiveStrength() * 2);
-                unitLoc[2][0].setDefenses(ds);
-                //Archer cannot move
-                unitLoc[2][0].setMoveCount(0);
-            }
-        }
-        //Check if blue archer
-        if (p == unitLoc[0][2]) {
-            //Check if already fortified
-            int oldDS = unitLoc[1][1].getDefensiveStrength() * 2;
-            if (oldDS == unitLoc[0][2].getDefensiveStrength()) {
-                //Defensive strength stays same
-                unitLoc[1][1].getDefensiveStrength();
-            }
-            //Double defensive strength
-            int ds = (unitLoc[0][2].getDefensiveStrength()) * 2;
-            unitLoc[0][2].setDefenses(ds);
-            //Archer cannot move
-            unitLoc[0][2].setMoveCount(0);
-        }*/
-        //Check if ufo
-        if (p.getRow() == 1 && p.getColumn() == 1) {
-            //Decrease city population by 1
-            ((CityImpl)c[1][1]).setSize(c[1][1].getSize() - 1);
-        }
+        //Get row, column, and unit
+        int r = p.getRow();
+        int col = p.getColumn();
 
-        //Check if forest
-        if (Objects.equals(t[p.getRow()][p.getColumn()].getTypeString(), GameConstants.FOREST)) {
-            ((TileImpl)t[p.getRow()][p.getColumn()]).setTypeString(GameConstants.PLAINS);
+        //Check if unit is null
+        if (u[r][col] != null) {
+            //Check if settler
+            if (Objects.equals(u[r][col].getTypeString(), GameConstants.SETTLER)) {
+                Player player = u[r][col].getOwner();
+                //Remove from world
+                u[r][col] = null;
+                //Build city with same owner
+                c[r][col] = new CityImpl(player);
+            }
+
+            //Check if archer
+            else if (Objects.equals(u[r][col].getTypeString(), GameConstants.ARCHER)) {
+                //Check if red
+                if (Objects.equals(u[r][col].getOwner(), Player.RED)) {
+                    //Check if already fortified
+                    int oldDS = 0;
+                    if ((r == 1) && (col == 0)) {
+                        oldDS = u[r][col].getDefensiveStrength() * 2;
+                    }
+                    if (oldDS == u[2][0].getDefensiveStrength()) {
+                        //Defensive strength stays same
+                        u[1][0].getDefensiveStrength();
+                    } else {
+                        //Double defensive strength
+                        int ds = (u[2][0].getDefensiveStrength() * 2);
+                        u[2][0].setDefenses(ds);
+                        //Archer cannot move
+                        u[2][0].setMoveCount(0);
+                    }
+                }
+
+                //Check if blue
+                else if (Objects.equals(u[r][col].getOwner(), Player.BLUE)) {
+                    //Check if already fortified
+                    int oldDS = 0;
+                    if ((r == 1) && (col == 1)) {
+                        oldDS = u[1][1].getDefensiveStrength() * 2;
+                    }
+                    if (oldDS == u[0][2].getDefensiveStrength()) {
+                        //Defensive strength stays same
+                        u[1][1].getDefensiveStrength();
+                    } else {
+                        //Double defensive strength
+                        int ds = (u[0][2].getDefensiveStrength()) * 2;
+                        u[0][2].setDefenses(ds);
+                        //Archer cannot move
+                        u[0][2].setMoveCount(0);
+                    }
+                }
+            }
+
+            //Check if ufo
+            if (Objects.equals(u[r][col].getTypeString(), GameConstants.UFO)) {
+                //Decrease city population by 1
+                ((CityImpl) c[1][1]).setSize(c[1][1].getSize() - 1);
+            }
+
+            //Check if forest
+            if (Objects.equals(t[p.getRow()][p.getColumn()].getTypeString(), GameConstants.FOREST)) {
+                ((TileImpl) t[p.getRow()][p.getColumn()]).setTypeString(GameConstants.PLAINS);
+            }
         }
     }
 }
