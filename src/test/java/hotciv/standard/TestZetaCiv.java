009510b3bf4ecb7736a62c20fdcae7a4898f8d9a
lukespowerconverters <res175@pitt.edu>
2021-11-13 21:41:58 -0500

    all testing suites currently pass

--- src/test/java/hotciv/standard/TestZetaCiv.java
+++ src/test/java/hotciv/standard/TestZetaCiv.java
@@ -20,8 +20,8 @@ public class TestZetaCiv {
         @Test public void roundLessThan20BetaWin() {
             int roundCount = 1;
             WinnerStrategyContext context = new WinnerStrategyContext();
-            Position rC=new Position(1,1);
-            Position bC=new Position(4,1);
+            Position rC = new Position(1,1);
+            Position bC = new Position(4,1);
             WinStrat ws = new ZetaWin(new BetaWin(), new EpsilonWin(), roundCount);
             assertThat(game.getWinner(), is(ws.getWinner(game)));
         }
@@ -29,8 +29,8 @@ public class TestZetaCiv {
        @Test public void roundGreaterThan20EpsilonWin() {
             int roundCount = 21;
            WinnerStrategyContext context = new WinnerStrategyContext();
-           Position rC=new Position(1,1);
-           Position bC=new Position(4,1);
+           Position rC = new Position(1,1);
+           Position bC = new Position(4,1);
            WinStrat ws = new ZetaWin(new BetaWin(), new EpsilonWin(), roundCount);
            assertThat(game.getWinner(), is(ws.getWinner(context,game)));
         }
