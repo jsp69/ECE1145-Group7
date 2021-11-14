f20ac89ab896bd83d5fd155a10d682abf40554cb
lukespowerconverters <res175@pitt.edu>
2021-11-13 21:35:57 -0500

    updated testing suite for GammaCiv

--- src/test/java/hotciv/standard/TestGammaCiv.java
+++ src/test/java/hotciv/standard/TestGammaCiv.java
@@ -3,6 +3,9 @@ package hotciv.standard;
 import hotciv.framework.*;
 import org.hamcrest.CoreMatchers;
 import org.junit.*;
+
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -22,9 +25,10 @@ public class TestGammaCiv {
     public void redSettlerBuildsCity() {
         Position redSettler = new Position(4, 3);
         assertThat(game, is(notNullValue()));
-        if (redSettler.equals("settler")) {
+        if (Objects.equals(game.getUnitAt(redSettler).getTypeString(), GameConstants.SETTLER)) {
+            game.performUnitActionAt(redSettler);
             //Check if red settler is removed
-            assertNull(((GameImpl)(game)).unitLoc[2][0]);
+            assertNull(game.getUnitAt(redSettler));
             //Check if red owns built city at p
             assertThat(game.getCityAt(redSettler).getOwner(), is(Player.RED));
             //Check if city population is size one
@@ -36,9 +40,10 @@ public class TestGammaCiv {
     public void blueSettlerBuildsCity() {
         Position blueSettler = new Position(3, 4);
         assertThat(game, is(notNullValue()));
-        if (blueSettler.equals("settler")) {
+        if (Objects.equals(game.getUnitAt(blueSettler).getTypeString(), GameConstants.SETTLER)) {
+            game.performUnitActionAt(blueSettler);
             //Check if blue settler is removed
-            assertNull(((GameImpl)(game)).unitLoc[3][4]);
+            assertNull(game.getUnitAt(blueSettler));
             //Check if blue owns built city at p
             assertThat(game.getCityAt(blueSettler).getOwner(), is(Player.BLUE));
             //Check if city population is size one
@@ -48,30 +53,29 @@ public class TestGammaCiv {
 
     @Test
     public void redArcherFortifies() {
-        Position oldRedArcher = new Position(1, 0);
-        Position newRedArcher = new Position(2, 0);
+        Position redArcher = new Position(2, 0);
         assertThat(game, is(notNullValue()));
-        if (newRedArcher.equals("archer")) {
+        if (Objects.equals(game.getUnitAt(redArcher).getTypeString(), GameConstants.ARCHER)) {
+            int oldDefStrength = game.getUnitAt(redArcher).getDefensiveStrength();
+            game.performUnitActionAt(redArcher);
             //Check if defensive strength doubles
-            int oldDoubled = game.getUnitAt(oldRedArcher).getDefensiveStrength() * 2;
-            assertThat(oldDoubled, is(game.getUnitAt(newRedArcher).getDefensiveStrength()));
+            assertThat(oldDefStrength * 2, is(game.getUnitAt(redArcher).getDefensiveStrength()));
             //Check move count set to 0
-            assertThat(game.getUnitAt(newRedArcher).getMoveCount(), is(0));
+            assertThat(game.getUnitAt(redArcher).getMoveCount(), is(0));
         }
     }
 
     @Test
     public void blueArcherFortifies() {
-        Position oldBlueArcher = new Position(1, 1);
-        Position newBlueArcher = new Position(0, 2);
+        Position blueArcher = new Position(0, 2);
         assertThat(game, is(notNullValue()));
-        if (newBlueArcher.equals("archer")) {
+        if (Objects.equals(game.getUnitAt(blueArcher).getTypeString(), GameConstants.ARCHER)) {
+            int oldDefStrength = game.getUnitAt(blueArcher).getDefensiveStrength();
+            game.performUnitActionAt(blueArcher);
             //Check if defensive strength doubles
-            int oldDoubled = game.getUnitAt(oldBlueArcher).getDefensiveStrength() * 2;
-            assertThat(oldDoubled, is(game.getUnitAt(newBlueArcher).getDefensiveStrength()));
+            assertThat(oldDefStrength * 2, is(game.getUnitAt(blueArcher).getDefensiveStrength()));
             //Check move count set to 0
-            assertThat(game.getUnitAt(newBlueArcher).getMoveCount(), is(0));
+            assertThat(game.getUnitAt(blueArcher).getMoveCount(), is(0));
         }
-        assertThat(game.getPlayerInTurn(), CoreMatchers.is(Player.RED));
     }
 }
