2e458453095963beef0741f948496090f6fc1a32
Bella <veb20@pitt.edu>
2021-12-05 23:01:23 -0500

    implemented addObserver(Gameobserver observer), added Gameobserver array

--- src/main/java/hotciv/standard/GameImpl.java
+++ src/main/java/hotciv/standard/GameImpl.java
@@ -2,34 +2,36 @@ package hotciv.standard;
 
 import hotciv.framework.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /** Skeleton implementation of HotCiv.
- 
-   This source code is from the book 
-     "Flexible, Reliable Software:
-       Using Patterns and Agile Development"
-     published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
-     Department of Computer Science
-     Aarhus University
-   
-   Please visit http://www.baerbak.com/ for further information.
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
- 
-       http://www.apache.org/licenses/LICENSE-2.0
- 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+ This source code is from the book
+ "Flexible, Reliable Software:
+ Using Patterns and Agile Development"
+ published 2010 by CRC Press.
+ Author:
+ Henrik B Christensen
+ Department of Computer Science
+ Aarhus University
 
-*/
+ Please visit http://www.baerbak.com/ for further information.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
 
 public class GameImpl implements Game {
 
@@ -55,6 +57,9 @@ public class GameImpl implements Game {
   CityStrat cityStrat;
   WorldStrat worldStrat;
   UnitActionStrat unitStrat;
+
+  // Observer array list
+  private List<GameObserver> obsList = new ArrayList<>();
 
   public GameImpl(HotCivFactory factory){
     //Use Abstract Factory object to generate variant method algorithms
@@ -153,7 +158,7 @@ public class GameImpl implements Game {
 
   @Override
   public void addObserver(GameObserver observer) {
-
+    obsList.add(observer);
   }
 
   @Override
