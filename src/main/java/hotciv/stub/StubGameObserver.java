c7f5ab197d568cafb4ec42f173c0e3e5cdb51b4c
Bella <veb20@pitt.edu>
2021-12-05 23:02:27 -0500

    added StubGameObserver for GameObserver Test Stub implementation

--- src/main/java/hotciv/stub/StubGameObserver.java
+++ src/main/java/hotciv/stub/StubGameObserver.java
@@ -1,2 +1,30 @@
-package hotciv.stub;public class StubGameObserver {
+package hotciv.stub;
+
+import hotciv.framework.GameObserver;
+import hotciv.framework.Player;
+import hotciv.framework.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
+public class StubGameObserver implements GameObserver {
+    private Position pos;
+    private Player nextPlayer;
+    private int age;
+    private Position position;
+
+    @Override
+    public void worldChangedAt(Position pos) {
+        this.pos = pos;
+    }
+    @Override
+    public void turnEnds(Player nextPlayer, int age) {
+        this.nextPlayer = nextPlayer;
+        this.age = age;
+    };
+    @Override
+    public void tileFocusChangedAt(Position position) {
+        this.position = position;
+    };
 }
