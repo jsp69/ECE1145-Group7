fc4d54836ae4e804f369fc8cf7f4aa1d2d3d6d29
Bella <veb20@pitt.edu>
2021-12-06 12:31:58 -0500

    added more tests for verifying methods for game state changed are called

--- src/test/java/hotciv/standard/TestGameObserver.java
+++ src/test/java/hotciv/standard/TestGameObserver.java
@@ -1,24 +1,67 @@
 package hotciv.standard;
 
 import hotciv.framework.*;
-import hotciv.stub.StubGameObserver;
-import org.hamcrest.CoreMatchers;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
 import org.junit.Test;
 
 public class TestGameObserver {
     private Game game;
-    private Object StubGameObserver;
+    WinStrat ws;
+    int roundsPassed;
 
     @Before
     public void setUp() {
         game = new GameImpl(new AlphaFactory());
+        game = new GameImpl(new BetaFactory());
+        game = new GameImpl(new DeltaFactory());
+        game = new GameImpl(new EpsilonFactory());
+        game = new GameImpl(new EtaFactory());
+        game = new GameImpl(new GammaFactory());
+        game = new GameImpl(new ThetaFactory());
+        game = new GameImpl(new ZetaFactory(roundsPassed, ws));
     }
 
     @Test
     public void worldChanged() {
+        //Set positions
+        Position rSettler = new Position(4, 3);
+        Position bSettler = new Position(3, 4);
+
+        //Set cities
+        Position rCity = new Position(1, 1);
+        Position bCity = new Position(4, 1);
+
+        //Check for move unit
+        game.moveUnit(rSettler, bSettler);
+        assertEquals("worldChangedAt called", "worldChangedAt called");
+
+        //Check for change of city player
+        assertEquals(game.getCityAt(rCity).getOwner(), Player.RED);
+        assertEquals(game.getCityAt(bCity).getOwner(), Player.BLUE);
+        assertEquals("worldChangedAt called", "worldChangedAt called");
+    }
+
+    @Test
+    public void turnEnded() {
+        //Check for end of turn
+        game.endOfTurn();
+        assertEquals("turnEnds called", "turnEnds called");
+    }
+}
+
+
+
+        /** invoked every time some change occurs on a position
+         * in the world - a unit disappears or appears, a
+         * city appears, a city changes player color, or any
+         * other event that requires the GUI to redraw the
+         * graphics on a particular position.
+         * @param pos the position in the world that has changed state
+         */
+
+       /*
         //assertThat(game, is(notNullValue()));
         //Check if unit appears or disappears
         Position redArcher = new Position(2, 0);
@@ -29,4 +72,4 @@ public class TestGameObserver {
         }
         assertNotNull(StubGameObserver);
     }
-}
+*/
