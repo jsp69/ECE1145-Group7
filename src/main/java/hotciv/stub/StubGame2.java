88996e7b3379955f7e32c104d9191fd575ecdac8
Bella <veb20@pitt.edu>
2021-12-11 16:36:24 -0500

    stub behavior test for performUnitActionAt()

--- src/main/java/hotciv/stub/StubGame2.java
+++ src/main/java/hotciv/stub/StubGame2.java
@@ -10,29 +10,29 @@ import java.util.*;
  *
  * SWEA support code.
  *
-   This source code is from the book 
-     "Flexible, Reliable Software:
-       Using Patterns and Agile Development"
-     published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
-     Department of Computer Science
-     Aarhus University
-   
-   Please visit http://www.baerbak.com/ for further information.
+ This source code is from the book
+ "Flexible, Reliable Software:
+ Using Patterns and Agile Development"
+ published 2010 by CRC Press.
+ Author:
+ Henrik B Christensen
+ Department of Computer Science
+ Aarhus University
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
- 
-       http://www.apache.org/licenses/LICENSE-2.0
- 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ Please visit http://www.baerbak.com/ for further information.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 public class StubGame2 implements Game {
 
@@ -64,7 +64,7 @@ public class StubGame2 implements Game {
   }
 
   // Stub only allows moving red archer
-  public boolean moveUnit( Position from, Position to ) { 
+  public boolean moveUnit( Position from, Position to ) {
     System.out.println( "-- StubGame2 / moveUnit called: "+from+"->"+to );
     if ( from.equals(pos_archer_red) ) {
       pos_archer_red = to;
@@ -72,7 +72,7 @@ public class StubGame2 implements Game {
     // notify our observer(s) about the changes on the tiles
     gameObserver.worldChangedAt(from);
     gameObserver.worldChangedAt(to);
-    return true; 
+    return true;
   }
 
   // === Turn handling ===
@@ -80,23 +80,23 @@ public class StubGame2 implements Game {
   public void endOfTurn() {
     System.out.println( "-- StubGame2 / endOfTurn called." );
     inTurn = (getPlayerInTurn() == Player.RED ?
-              Player.BLUE : 
-              Player.RED );
+            Player.BLUE :
+            Player.RED );
     // no age increments
     gameObserver.turnEnds(inTurn, -4000);
   }
   public Player getPlayerInTurn() { return inTurn; }
-  
+
 
   // === Observer handling ===
   protected GameObserver gameObserver;
   // observer list is only a single one...
   public void addObserver(GameObserver observer) {
     gameObserver = observer;
-  } 
+  }
 
-  public StubGame2() { 
-    defineWorld(1); 
+  public StubGame2() {
+    defineWorld(1);
     // AlphaCiv configuration
     pos_archer_red = new Position( 2, 0);
     pos_legion_blue = new Position( 3, 2);
@@ -112,7 +112,7 @@ public class StubGame2 implements Game {
   }
 
   // A simple implementation to draw the map of DeltaCiv
-  protected Map<Position,Tile> world; 
+  protected Map<Position,Tile> world;
   public Tile getTileAt( Position p ) { return world.get(p); }
 
 
@@ -133,10 +133,10 @@ public class StubGame2 implements Game {
   // TODO: Add more stub behaviour to test MiniDraw updating
   public City getCityAt( Position p ) { if(p.getRow()==4 && p.getColumn()==0) return red_city; else return null; }
   public Player getWinner() { return null; }
-  public int getAge() { return 0; }  
+  public int getAge() { return 0; }
   public void changeWorkForceFocusInCityAt( Position p, String balance ) {}
   public void changeProductionInCityAt( Position p, String unitType ) {}
-  public void performUnitActionAt( Position p ) {}  
+  public void performUnitActionAt( Position p ) {gameObserver.worldChangedAt(p);}
 
   public void setTileFocus(Position position) {
     gameObserver.tileFocusChangedAt(position);
